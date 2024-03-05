b6212631ecd49db65793b1f6f5546cbc5a1ae38d
Yaakov Meir <ymeir02@gmail.com>
2024-03-05 18:08:22 -0300

    inciando projeto e modelo de navegação

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import Navigator from "./routes/Routes";
+import { Navbar } from "./components/navbar/Navbar";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    <Navigator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Home(){
+    <section>
+      <h1>OI</h1>
+    
+    </section>
+}
+export default Home
